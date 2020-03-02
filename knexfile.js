feef8ef59bc508e8f042b4a9dbdb9f5dca71304c
Henrik Nygren <nygrenh@gmail.com>
2020-03-02 21:12:49 +0200

    Automatically run migrations

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
       database: process.env.POSTGRES_DATABASE,
       host: process.env.POSTGRES_HOST,
       port: process.env.POSTGRES_PORT,
-      username: process.env.POSTGRES_USERNAME,
+      user: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
     },
     pool: {
