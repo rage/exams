74c3fcca1c5b550f70b1b79cce9603a81466438a
Henrik Nygren <nygrenh@gmail.com>
2020-05-20 17:18:56 +0300

    Migrate front page to swr

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -1,17 +1,21 @@
 import React, { useContext } from "react"
 import { NextPage } from "next"
-import { Typography, Card, Button, ButtonBase, NoSsr } from "@material-ui/core"
+import { Typography, Card, ButtonBase, NoSsr } from "@material-ui/core"
 import { DateTime } from "luxon"
 import styled from "styled-components"
 import Link from "next/link"
 import { SimpleExam, fetchExams } from "../../services/api"
-import { withLoggedIn } from "../../contexes/LoginStateContext"
+import LoginStateContext, {
+  withLoggedIn,
+} from "../../contexes/LoginStateContext"
 import getAccessToken from "../../lib/getAccessToken"
 import { useTime } from "../../hooks/useTime"
 import Layout from "../../components/Layout"
 import withLocale from "../../lib/withLocale"
 import useTranslator from "../../hooks/useTranslator"
 import { LocaleContext } from "../../contexes/LocaleContext"
+import Skeleton from "@material-ui/lab/Skeleton"
+import useSwr from "swr"
 
 interface PageProps {
   exams: SimpleExam[]
@@ -32,11 +36,25 @@ const StyledLink = styled.a`
   cursor: pointer;
 `
 
-const Page: NextPage<PageProps> = ({ exams }) => {
+const StyledSkeleton = styled(Skeleton)`
+  border-radius: 0.25rem;
+`
+
+const Page: NextPage = () => {
   const t = useTranslator()
   const { locale } = useContext(LocaleContext)
   const now = useTime()
   const currentDateString = now.toLocaleString(DateTime.DATE_FULL)
+  const { accessToken } = useContext(LoginStateContext)
+  const { data, error } = useSwr(accessToken, (token) => fetchExams(token))
+
+  if (error) {
+    return (
+      <Layout>
+        Error: <pre>{JSON.stringify(error, undefined, 2)}</pre>
+      </Layout>
+    )
+  }
 
   return (
     <Layout>
@@ -46,53 +64,63 @@ const Page: NextPage<PageProps> = ({ exams }) => {
 
       <br />
 
-      {exams
-        .filter((o) => {
-          const start = DateTime.fromISO(o.starts_at)
-          const end = DateTime.fromISO(o.ends_at)
-          return (
-            start.toLocaleString(DateTime.DATE_FULL) === currentDateString ||
-            (now > start && now < end)
+      {!data && (
+        <div role="alert" aria-valuetext="Loading..." aria-busy="true">
+          <StyledSkeleton variant="rect" height={88} />
+          <br />
+          <StyledSkeleton variant="rect" height={88} />
+        </div>
+      )}
+
+      {data &&
+        data
+          .filter((o) => {
+            const start = DateTime.fromISO(o.starts_at)
+            const end = DateTime.fromISO(o.ends_at)
+            return (
+              start.toLocaleString(DateTime.DATE_FULL) === currentDateString ||
+              (now > start && now < end)
+            )
+          })
+          .sort((o1, o2) =>
+            DateTime.fromISO(o1.starts_at)
+              .diff(DateTime.fromISO(o2.starts_at))
+              .as("seconds"),
           )
-        })
-        .sort((o1, o2) =>
-          DateTime.fromISO(o1.starts_at)
-            .diff(DateTime.fromISO(o2.starts_at))
-            .as("seconds"),
-        )
-        .map((o) => {
-          return (
-            <Link
-              key={o.id}
-              href={`${locale}/exams/[id].tsx`}
-              as={`${locale}/exams/${o.id}`}
-            >
-              <StyledLink>
-                <StyledButtonBase component="div">
-                  <ExamCard>
-                    <Typography variant="h5" component="h2">
-                      {o.name}
-                    </Typography>
-                    <Typography>
-                      <NoSsr>
-                        {DateTime.fromISO(o.starts_at).toLocaleString(
-                          DateTime.DATE_FULL,
-                        )}
-                      </NoSsr>
-                    </Typography>
-                  </ExamCard>
-                </StyledButtonBase>
-              </StyledLink>
-            </Link>
-          )
-        })}
+          .map((o) => {
+            return (
+              <Link
+                key={o.id}
+                href={`${locale}/exams/[id].tsx`}
+                as={`${locale}/exams/${o.id}`}
+              >
+                <StyledLink>
+                  <StyledButtonBase component="div">
+                    <ExamCard>
+                      <Typography variant="h5" component="h2">
+                        {o.name}
+                      </Typography>
+                      <Typography>
+                        <NoSsr>
+                          {DateTime.fromISO(o.starts_at).toLocaleString(
+                            DateTime.DATE_FULL,
+                          )}
+                        </NoSsr>
+                      </Typography>
+                    </ExamCard>
+                  </StyledButtonBase>
+                </StyledLink>
+              </Link>
+            )
+          })}
     </Layout>
   )
 }
 
 Page.getInitialProps = async (ctx) => {
-  const exams = await fetchExams(getAccessToken(ctx))
-  return { exams }
+  // const exams = await fetchExams(getAccessToken(ctx))
+  // return { exams }
+  return {}
 }
 
 export default withLocale(withLoggedIn(Page))
